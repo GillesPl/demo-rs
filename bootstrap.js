4fdbf7acb40c6956da9020c4745dc11dc1374bf1
Maarten <maarten.somers@gmail.com>
2018-04-18 17:08:27 +0200

    Stop serving FE via Node

--- bootstrap.js
+++ bootstrap.js
@@ -21,108 +21,97 @@ const _ = require('lodash');
 let app;
 
 if (!process.env.hasOwnProperty('NODE_ENV')){
-    //default to development environment
-    process.env.NODE_ENV = 'development';
+  //default to development environment
+  process.env.NODE_ENV = 'development';
 }
 
 function setupEnv() {
-    if ( process.env.NODE_ENV.toLowerCase() !== 'test') {
-        process.env.NODE_ENV = config.environment;
-    }
+  if ( process.env.NODE_ENV.toLowerCase() !== 'test') {
+    process.env.NODE_ENV = config.environment;
+  }
 }
 
 function loadRoutes() {
-    const routes = glob.sync('./server/**/*.router.js');
-    routes.forEach((file) => require(file)(app));
+  const routes = glob.sync('./server/**/*.router.js');
+  routes.forEach((file) => require(file)(app));
 }
 
 function initializeExpress(callback) {
-    app = express();
-    app.set('port', process.env.PORT || config.port);
-    app.use(helmet());
+  app = express();
+  app.set('port', process.env.PORT || config.port);
+  app.use(helmet());
 
-    // Use compression to save bandwidth
-    if (process.env.NODE_ENV !== 'test') {
-        app.use(require('compression')());
+  // Use compression to save bandwidth
+  if (process.env.NODE_ENV !== 'test') {
+    app.use(require('compression')());
+  }
+
+  blocked(function (ms) {
+    if (ms < 50) {
+      logger.info('Event loop blocked for ' + ms + 'ms');
+    } else if (ms < 100) {
+      logger.log('Event loop blocked for ' + ms + 'ms');
+    } else {
+      logger.warn('Event loop blocked for ' + ms + 'ms');
     }
+  });
 
-    blocked(function (ms) {
-        if (ms < 50) {
-            logger.info('Event loop blocked for ' + ms + 'ms');
-        } else if (ms < 100) {
-            logger.log('Event loop blocked for ' + ms + 'ms');
-        } else {
-            logger.warn('Event loop blocked for ' + ms + 'ms');
-        }
-    });
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json({ limit: '4096kb' }));
 
-    app.use(bodyParser.urlencoded({ extended: false }));
-    app.use(bodyParser.json({ limit: '4096kb' }));
+  // Uncomment to enable request logging
+  // app.use(logger.middleware);
 
-    // Uncomment to enable request logging
-    // app.use(logger.middleware);
+  app.set('views', path.join(__dirname + '/server/', 'views'));
+  app.set('view engine', 'ejs');
 
-    app.set('views', path.join(__dirname + '/server/', 'views'));
-    app.set('view engine', 'ejs');
+  // Include routes if exists
+  loadRoutes();
 
-    // Include routes if exists
-    loadRoutes();
+// Catch all other routes and return the index file
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  });
 
 
-    // Set public folder
-    if (_.includes(DEV_ENVS, process.env.NODE_ENV.toLowerCase())) {
-        app.use('/' , express.static(__dirname + '/.local'));
-        app.use('/node_modules', express.static(__dirname + '/node_modules'));
-    } else app.use('/' , express.static(__dirname + '/dist'));
-
-
-    // Error handling middleware should be loaded after the loading the routes
-    if (_.includes(DEV_ENVS, process.env.NODE_ENV.toLowerCase())) {
-        app.use(require('morgan')('dev'));
-        app.use(require('errorhandler')());
-    }
-
-    // Redirect unmatched requests to index
-    app.all('*', redirectUnmatched);
-
-    return callback();
+  return callback();
 }
 
 function redirectUnmatched(req, res) {
-    if (config.redirect.port) res.redirect(config.redirect.scheme + '://' + config.redirect.domain + ':' + config.redirect.port);
-    else res.redirect(config.redirect.scheme + '://' + config.redirect.domain);
+  if (config.redirect.port) res.redirect(config.redirect.scheme + '://' + config.redirect.domain + ':' + config.redirect.port);
+  else res.redirect(config.redirect.scheme + '://' + config.redirect.domain);
 }
 
 function startListening(callback) {
-    app.listen(app.get('port'), function onListen() {
-        //noinspection JSAnnotator
-        logger.log(`\n ${chalk.white.bgGreen.bold(' T1T ')} ${chalk.grey.bgBlack(' Express server listening on port ' + app.get('port'))}`);
-        return callback();
-    });
+  app.listen(app.get('port'), function onListen() {
+    //noinspection JSAnnotator
+    logger.log(`\n ${chalk.white.bgGreen.bold(' T1T ')} ${chalk.grey.bgBlack(' Express server listening on port ' + app.get('port'))}`);
+    return callback();
+  });
 }
 
 function start(cb) {
-    setupEnv();
-    // give some time for pre-initialization
-    async.series([
-        initializeExpress,
-        startListening
-    ], (err) => {
-        if (err) {
-            logger.error('Error occurred' + err);
-            return process.exit(1);
-        }
-        if (cb && typeof cb === 'function') {
-        return cb(err);
+  setupEnv();
+  // give some time for pre-initialization
+  async.series([
+    initializeExpress,
+    startListening
+  ], (err) => {
+    if (err) {
+      logger.error('Error occurred' + err);
+      return process.exit(1);
     }
-});
+    if (cb && typeof cb === 'function') {
+      return cb(err);
+    }
+  });
 }
 
 function stop() {
-    app.close();
+  app.close();
 }
 
 module.exports = {
-    start: start,
-    stop: stop
+  start: start,
+  stop: stop
 };
