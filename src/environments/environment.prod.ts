bdbbae2fb360ef48a1f36dcf900c737393b3fd0a
Maarten <maarten.somers@gmail.com>
2018-04-27 15:26:55 +0200

    Remove apikey from environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,6 @@ export const environment = {
   production: false,
   name: 'ReadMyCards',
   version: '2.0.0',
-  apiKey: '7de3b216-ade2-4391-b2e2-86b80bac4d7d',
   gwOrProxyUrl: 'https://apim.t1t.be:443',
   gclUrl: 'https://localhost:10443/v2',
   implicitDownload: false,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,6 @@ export const environment = {
   production: false,
   name: 'ReadMyCards',
   version: '2.0.0',
-  apiKey: '7de3b216-ade2-4391-b2e2-86b80bac4d7d',
   gwOrProxyUrl: 'https://accapim.t1t.be:443',
   gclUrl: 'https://localhost:10443/v2',
   implicitDownload: false,
