4e6efb841e48e5a3cb42bd1305bf2b76263828cb
Gilles <platteeuw.gilles@gmail.com>
2018-08-17 15:01:26 +0200

    using ocvjwt

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   gwOrProxyUrl: 'https://apim.t1t.be:443',
   gclUrl: 'https://localhost:10443/v2',
   dsContextPath: '/trust1team/gclds/v2',
-  ocvContextPath: '/trust1team/ocv/v1',
+  ocvContextPath: '/trust1team/ocvjwt/v1',
   implicitDownload: false,
   osPinDialog: true,
   consentCodeLength: 8,
